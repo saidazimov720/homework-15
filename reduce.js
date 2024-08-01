c34133184d47060573117fc429ae0800e5834994
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-08-01 16:48:53 +0500

    for loop

--- reduce.js
+++ reduce.js
@@ -1,4 +1,9 @@
 function reduce(arr, cb, initVal) {
     let acc = initVal !== undefined ? initVal  : arr[0];
     let startId = initVal != undefined ? 0:1;
+
+    for (let i = startId; i < arr.length; i++) {
+        acc = cb(acc, arr[i], i, arr);
+    }
+    
 }
