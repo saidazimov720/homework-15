a1ec63da0f0bffc4127e356b9ba7596a48841dfc
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-08-01 16:52:54 +0500

    arrray numbers and arrrow function

--- reduce.js
+++ reduce.js
@@ -5,5 +5,9 @@ function reduce(arr, cb, initVal) {
     for (let i = startId; i < arr.length; i++) {
         acc = cb(acc, arr[i], i, arr);
     }
-    
+    return acc;
 }
+
+const num = [1,2,3,4,5,6,7];
+const sum = reduce(num, (acc, currNum) => acc + currNum, 0);
+console.log(sum);
